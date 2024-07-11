4da86eae449bfbba121cc1425317ba7a568b0289
Abiodun Uthman Allison <allison.uthman@toglab.com>
2024-07-11 13:01:49 +0100

    add langgraph-cloud test, lesson 2 - 6

--- cloud/agent.py
+++ cloud/agent.py
@@ -2,7 +2,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 
-model = ChatAnthropic(model_namet="claude-3-5-sonnet-20240620") # type: ignore
+model = ChatAnthropic(model_name="claude-3-5-sonnet-20240620") # type: ignore
 
 tools = [TavilySearchResults(max_results=2)]
 
